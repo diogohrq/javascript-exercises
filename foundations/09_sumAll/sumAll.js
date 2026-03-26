ffee396d5500e709c5bb8e9dfe271e78983a8c72
diogohrq <69657046+diogohrq@users.noreply.github.com>
2026-03-25 21:58:09 -0300

    Complete foundations/09_sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if ((num1 < 0 || num2 < 0) || (!Number.isInteger(num1) || !Number.isInteger(num2))) {
+    return 'ERROR'
+  }
 
+  let lowestNumber = 0;
+  let highestNumber = 0;
+  let sumTotal = 0;
+
+  if (num1 < num2) {
+    lowestNumber = num1;
+    highestNumber = num2;
+  } else {
+    lowestNumber = num2;
+    highestNumber = num1;
+  }
+
+  for (let i = lowestNumber; i <= highestNumber; i++) {
+    sumTotal += i;
+  }
+
+  return sumTotal;
 };
 
 // Do not edit below this line
