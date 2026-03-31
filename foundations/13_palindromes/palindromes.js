f44492e6566d8e26992470d2276e4cbd5d2cf9fb
diogohrq <69657046+diogohrq@users.noreply.github.com>
2026-03-31 11:48:35 -0300

    Complete foundation/13_palindromes exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string)  {
+  let reversedString = '';
 
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string[i].toLowerCase();
+  }
+
+  reversedString = reversedString.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  string = string.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+
+  return string == reversedString;
 };
 
 // Do not edit below this line
