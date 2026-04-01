4d8028ae5a073cb850c845e4f3bf6915edf41d78
diogohrq <69657046+diogohrq@users.noreply.github.com>
2026-04-01 20:19:44 -0300

    Complete foundations/16_findTheOldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const currentYear = new Date().getFullYear();
 
+  const oldest = people.reduce((obj, person) => {
+    const oldest = (obj.yearOfDeath ?? currentYear)  - obj.yearOfBirth;
+    const currentAge = (person.yearOfDeath ?? currentYear) - person.yearOfBirth;
+
+    if (currentAge > oldest) {
+      return person;
+    } else {
+      return obj;
+    }
+  }, people[0]);
+
+  return oldest;
 };
 
 // Do not edit below this line
