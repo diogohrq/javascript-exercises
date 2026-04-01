9ff663167e61292de05ec1c85e43a39b4226732f
diogohrq <69657046+diogohrq@users.noreply.github.com>
2026-03-31 21:03:06 -0300

    Complete foundations/14_fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  if (number < 0) {
+    return 'OOPS';
+  }
 
+  if (number == 1 || number == 2) {
+    return 1;
+  }
+
+  let pos1 = 1;
+  let pos2 = 1;
+  let pos3 = 0;
+
+  for(let i = 0; i < number - 2; i++) {
+    pos3 = pos1 + pos2;
+    pos1 = pos2;
+    pos2 = pos3;
+  }
+
+  return pos3;
 };
 
 // Do not edit below this line
